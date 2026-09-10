4ae0c2ea34061ad0535f19fb94767d30a584518c
Emil Pieter Mora <94746253+w129@users.noreply.github.com>
2026-09-10 14:26:34 -0400

    Bump entry slogan asset version

--- components/admin-hello-button.js
+++ components/admin-hello-button.js
@@ -88,7 +88,7 @@
 
         if (!document.querySelector('script[data-platform-entry-slogan]')) {
             const sloganScript = document.createElement('script');
-            sloganScript.src = componentBase + 'platform-entry-slogan.js?v=20260910-1';
+            sloganScript.src = componentBase + 'platform-entry-slogan.js?v=20260910-2';
             sloganScript.defer = true;
             sloganScript.dataset.platformEntrySlogan = 'true';
             document.head.appendChild(sloganScript);

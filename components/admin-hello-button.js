5824faac93e8058749616231dc3c148ebe19d512
Emil Pieter Mora <94746253+w129@users.noreply.github.com>
2026-09-11 13:38:57 -0400

    Refresh vector tray script for second icon

--- components/admin-hello-button.js
+++ components/admin-hello-button.js
@@ -146,7 +146,7 @@
 
         if (!document.querySelector('script[data-platform-entry-slogan]')) {
             const sloganScript = document.createElement('script');
-            sloganScript.src = componentBase + 'platform-entry-slogan.js?v=20260911-1';
+            sloganScript.src = componentBase + 'platform-entry-slogan.js?v=20260911-2';
             sloganScript.defer = true;
             sloganScript.dataset.platformEntrySlogan = 'true';
             document.head.appendChild(sloganScript);

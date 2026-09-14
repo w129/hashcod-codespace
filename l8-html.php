afd0e30555eb833304999da97a59518a46ca2eeb
Emil Pieter Mora <94746253+w129@users.noreply.github.com>
2026-09-14 00:15:10 -0400

    Deploy percent button inside fourth vector tray cube

--- l8-html.php
+++ l8-html.php
@@ -121,7 +121,7 @@ function l8_require_html_page($file, $ok = true, $cacheTtl = 0) {
             . '<link rel="stylesheet" href="' . $base . 'components/duo-page-transition.css?v=20260913-2" data-hashcod-duo-transition-style="true">'
             . '<link rel="stylesheet" href="' . $base . 'components/platform-entry-capability-footer.css?v=20260913-3" data-hashcod-entry-capability-footer-style="true">'
             . '<link rel="stylesheet" href="' . $base . 'components/boot-brand-credit-relocate.css?v=20260913-1" data-hashcod-boot-brand-credit-relocate-style="true">'
-            . '<link rel="stylesheet" href="' . $base . 'components/percent-feature-button.css?v=20260913-3" data-hashcod-percent-feature-style="true">';
+            . '<link rel="stylesheet" href="' . $base . 'components/percent-feature-button.css?v=20260914-1" data-hashcod-percent-feature-style="true">';
 
         // If the previous page closed with Duo, mark this page before first paint
         // so it can open from the closed state without flashing the normal page.
@@ -195,8 +195,8 @@ function l8_require_html_page($file, $ok = true, $cacheTtl = 0) {
             . '<script defer src="' . $base . 'components/platform-entry-capability-footer.js?v=20260913-3" data-hashcod-entry-capability-footer="true"></script>'
             . '<script defer src="' . $base . 'components/platform-entry-capability-footer-fix.js?v=20260913-1" data-hashcod-entry-capability-footer-fix="true"></script>'
             . '<script defer src="' . $base . 'components/auth-tabs-rescue.js?v=20260913-3" data-hashcod-auth-tabs-rescue="true"></script>'
-            . '<script defer src="' . $base . 'components/percent-feature-button.js?v=20260913-3" data-hashcod-percent-feature="true"></script>'
-            . '<script defer src="' . $base . 'components/boot-brand-credit-relocate.js?v=20260913-3" data-hashcod-boot-brand-credit-relocate="true"></script>';
+            . '<script defer src="' . $base . 'components/percent-feature-button.js?v=20260914-1" data-hashcod-percent-feature="true"></script>'
+            . '<script defer src="' . $base . 'components/boot-brand-credit-relocate.js?v=20260914-1" data-hashcod-boot-brand-credit-relocate="true"></script>';
         $bodyPos = strripos($html, '</body>');
         if ($bodyPos !== false) {
             $html = substr($html, 0, $bodyPos) . $tag . substr($html, $bodyPos);

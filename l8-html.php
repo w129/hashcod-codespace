26d43aadf71d1f93ab855e26fa48208ca668f1e0
Emil Pieter Mora <94746253+w129@users.noreply.github.com>
2026-09-13 21:43:19 -0400

    Bump capability footer style after icon sizing fix

--- l8-html.php
+++ l8-html.php
@@ -119,7 +119,7 @@ function l8_require_html_page($file, $ok = true, $cacheTtl = 0) {
             . '<link rel="stylesheet" href="' . $base . 'components/toolbox-secure-links.css?v=20260913-3" data-hashcod-toolbox-secure-style="true">'
             . '<link rel="stylesheet" href="' . $base . 'components/admin-hello-button.css?v=20260913-sequence10" data-hashcod-boot-icons-style="true">'
             . '<link rel="stylesheet" href="' . $base . 'components/duo-page-transition.css?v=20260913-2" data-hashcod-duo-transition-style="true">'
-            . '<link rel="stylesheet" href="' . $base . 'components/platform-entry-capability-footer.css?v=20260913-1" data-hashcod-entry-capability-footer-style="true">';
+            . '<link rel="stylesheet" href="' . $base . 'components/platform-entry-capability-footer.css?v=20260913-2" data-hashcod-entry-capability-footer-style="true">';
 
         // If the previous page closed with Duo, mark this page before first paint
         // so it can open from the closed state without flashing the normal page.

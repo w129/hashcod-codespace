7400ea243100fac7f417372502cfea1a5725f130
Emil Pieter Mora <94746253+w129@users.noreply.github.com>
2026-09-14 00:53:09 -0400

    Refresh creator icon integration assets

--- l8-html.php
+++ l8-html.php
@@ -120,7 +120,7 @@ function l8_require_html_page($file, $ok = true, $cacheTtl = 0) {
             . '<link rel="stylesheet" href="' . $base . 'components/admin-hello-button.css?v=20260914-sequence15" data-hashcod-boot-icons-style="true">'
             . '<link rel="stylesheet" href="' . $base . 'components/duo-page-transition.css?v=20260913-2" data-hashcod-duo-transition-style="true">'
             . '<link rel="stylesheet" href="' . $base . 'components/platform-entry-capability-footer.css?v=20260913-3" data-hashcod-entry-capability-footer-style="true">'
-            . '<link rel="stylesheet" href="' . $base . 'components/boot-brand-credit-relocate.css?v=20260914-4" data-hashcod-boot-brand-credit-relocate-style="true">'
+            . '<link rel="stylesheet" href="' . $base . 'components/boot-brand-credit-relocate.css?v=20260914-5" data-hashcod-boot-brand-credit-relocate-style="true">'
             . '<link rel="stylesheet" href="' . $base . 'components/percent-feature-button.css?v=20260914-1" data-hashcod-percent-feature-style="true">';
 
         // If the previous page closed with Duo, mark this page before first paint
@@ -196,7 +196,7 @@ function l8_require_html_page($file, $ok = true, $cacheTtl = 0) {
             . '<script defer src="' . $base . 'components/platform-entry-capability-footer-fix.js?v=20260913-1" data-hashcod-entry-capability-footer-fix="true"></script>'
             . '<script defer src="' . $base . 'components/auth-tabs-rescue.js?v=20260913-3" data-hashcod-auth-tabs-rescue="true"></script>'
             . '<script defer src="' . $base . 'components/percent-feature-button.js?v=20260914-1" data-hashcod-percent-feature="true"></script>'
-            . '<script defer src="' . $base . 'components/boot-brand-credit-relocate.js?v=20260914-4" data-hashcod-boot-brand-credit-relocate="true"></script>';
+            . '<script defer src="' . $base . 'components/boot-brand-credit-relocate.js?v=20260914-5" data-hashcod-boot-brand-credit-relocate="true"></script>';
         $bodyPos = strripos($html, '</body>');
         if ($bodyPos !== false) {
             $html = substr($html, 0, $bodyPos) . $tag . substr($html, $bodyPos);

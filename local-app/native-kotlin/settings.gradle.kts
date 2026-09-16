693f43f2264e759367c4fab47112fcf6281195bf
Emil Pieter Mora <94746253+w129@users.noreply.github.com>
2026-09-16 18:07:08 -0400

    Add Kotlin native local app settings

---
+++ local-app/native-kotlin/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+rootProject.name = "hashcod-codespace-native-kotlin"

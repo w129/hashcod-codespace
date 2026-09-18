0956ad305a8491259fb6b0ae3c29480a38d82326
Emil Pieter Mora <94746253+w129@users.noreply.github.com>
2026-09-18 11:28:51 -0400

    Serve current entry controller in hosted app

--- l8-html.php
+++ l8-html.php
@@ -318,7 +318,7 @@ function l8_require_html_page($file, $ok = true, $cacheTtl = 0) {
         $tag = $legacyBlackholeCleanupTag
             . '<script defer src="' . $base . 'components/legacy-auth-retirement.js?v=20260917-1" data-hashcod-legacy-auth-retirement="true"></script>'
             . '<script defer src="' . $base . 'components/platform-entry-motion.js?v=20260918-1" data-platform-entry-motion="true"></script>'
-            . '<script defer src="' . $base . 'components/platform-entry-hold.js?v=20260918-2" data-platform-entry-hold="true"></script>'
+            . '<script defer src="' . $base . 'components/platform-entry-hold.js?v=20260918-3" data-platform-entry-hold="true"></script>'
             . '<script defer src="' . $base . 'components/platform-registration-form.js?v=20260918-2" data-hashcod-platform-registration="true"></script>'
             . $rareFolderInlineTag
             . $rareFolderExternalTag

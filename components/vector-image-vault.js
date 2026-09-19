93e379375865b03e4978bce18e1cb7bb5a42a021
Emil Pieter Mora <94746253+w129@users.noreply.github.com>
2026-09-19 09:16:26 -0400

    Load current CodeKey verifier

--- components/vector-image-vault.js
+++ components/vector-image-vault.js
@@ -31,7 +31,7 @@
         });
         if (!existing) {
             const script = document.createElement('script');
-            script.src = componentBase() + 'admin-device.js?v=20260918-codekey3';
+            script.src = componentBase() + 'admin-device.js?v=20260918-codekey4';
             script.defer = true;
             script.dataset.vectorImageVaultAdmin = 'true';
             document.head.appendChild(script);

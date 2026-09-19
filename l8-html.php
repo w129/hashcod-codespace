d41919b0202f07a47e04e4dc32cf82e21af922bf
Emil Pieter Mora <94746253+w129@users.noreply.github.com>
2026-09-19 00:17:47 -0400

    Serve persistent registration recovery assets

--- l8-html.php
+++ l8-html.php
@@ -364,8 +364,8 @@ function l8_require_html_page($file, $ok = true, $cacheTtl = 0) {
             . $registrationFlipTag
             . '<script defer src="' . $base . 'components/legacy-auth-retirement.js?v=20260918-2" data-hashcod-legacy-auth-retirement="true"></script>'
             . '<script defer src="' . $base . 'components/platform-entry-motion.js?v=20260918-1" data-platform-entry-motion="true"></script>'
-            . '<script defer src="' . $base . 'components/platform-entry-hold.js?v=20260918-32" data-platform-entry-hold="true"></script>'
-            . '<script defer src="' . $base . 'components/platform-registration-form.js?v=20260918-32" data-hashcod-platform-registration="true"></script>'
+            . '<script defer src="' . $base . 'components/platform-entry-hold.js?v=20260918-33" data-platform-entry-hold="true"></script>'
+            . '<script defer src="' . $base . 'components/platform-registration-form.js?v=20260918-33" data-hashcod-platform-registration="true"></script>'
             . $rareFolderInlineTag
             . $rareFolderExternalTag
             . $inlineRescueTag

839f0789dd109223e37ca3d154bb2fedeb2c875a
Emil Pieter Mora <94746253+w129@users.noreply.github.com>
2026-09-19 17:41:59 -0400

    Bust caches for scoped boot and auth layout runtimes

--- laragon-local-entry.php
+++ laragon-local-entry.php
@@ -144,7 +144,7 @@ $bodyExtras = '<script defer src="' . $baseAttr . 'components/legacy-auth-retire
     . '<script defer src="' . $baseAttr . 'components/admin-codekey-picker-rescue.js?v=20260918-4" data-hashcod-codekey-picker-rescue="true"></script>'
     . '<script defer src="' . $baseAttr . 'components/percent-feature-button.js?v=20260914-1" data-hashcod-percent-feature="true"></script>'
     . '<script defer src="' . $baseAttr . 'components/efr-code-editor.js?v=20260919-perf1" data-hashcod-efr-code-editor="true"></script>'
-    . '<script defer src="' . $baseAttr . 'components/boot-brand-credit-relocate.js?v=20260917-10" data-hashcod-boot-brand-credit-relocate="true"></script>'
+    . '<script defer src="' . $baseAttr . 'components/boot-brand-credit-relocate.js?v=20260919-perf1" data-hashcod-boot-brand-credit-relocate="true"></script>'
     . '<script defer src="' . $baseAttr . 'components/laragon-credit-align.js?v=20260914-local1" data-hashcod-laragon-credit-align="true"></script>';
 
 $bodyPos = strripos($html, '</body>');

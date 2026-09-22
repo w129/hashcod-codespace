7f4ad58d2e8639114a29006f67cb19799db22da9
Emil Pieter Mora <94746253+w129@users.noreply.github.com>
2026-09-22 11:54:03 -0400

    Add resources manifest JS seed

---
+++ resources/manifest.js
@@ -0,0 +1,8 @@
+window.HASHCOD_RESOURCES_MANIFEST = {
+  version: 1,
+  generated_at: '2026-09-22T11:46:00-04:00',
+  title: 'Hashcod Resources',
+  description: 'Índice público de recursos de Hashcod Codespace. Genera este archivo con scripts/upload_resources.py usando --js-output resources/manifest.js.',
+  count: 0,
+  resources: []
+};

155e1670d904874480ad69cc273b258bf42ef113
Emil Pieter Mora <94746253+w129@users.noreply.github.com>
2026-09-25 09:14:53 -0400

    Load HeroUI-style registration color picker

--- components/codespace-security-monitor.js
+++ components/codespace-security-monitor.js
@@ -69,16 +69,28 @@
     );
   }
 
+  function loadRegistrationHeroUIColorPicker() {
+    loadStylesheetOnce(
+      'hashcodRegistrationHeroUIColorPickerStylesheet',
+      '/components/hashcod-registration-heroui-colorpicker.css?v=20260925-colorpicker1'
+    );
+    loadScriptOnce(
+      'hashcodRegistrationHeroUIColorPickerBridge',
+      '/components/hashcod-registration-heroui-colorpicker.js?v=20260925-colorpicker1'
+    );
+  }
+
   function bootVisualCleanup() {
     removeLegacyBadge();
     installDockIconIntegrationStyle();
     loadRegistrationGlassTheme();
     loadRegistrationPixelBackground();
     loadRegistrationHeroUIDateField();
+    loadRegistrationHeroUIColorPicker();
   }
 
   window.HashcodSecurityMonitor = {
-    version: 'safe-shim-2026-09-25-registration-heroui-datefield2',
+    version: 'safe-shim-2026-09-25-registration-heroui-colorpicker',
     status: 'hardened',
     runFullAudit: function () {
       console.info('[Hashcod Security] Safe monitor active. No client-side command execution is enabled.');
